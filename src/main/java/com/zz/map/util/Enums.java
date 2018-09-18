03c77f99cab3a2bfc82b0c7f46676889854d17eb
zhuo95 <34971718+zhuo95@users.noreply.github.com>
2018-09-18 12:03:38 -0400

    add Enum util

---
+++ src/main/java/com/zz/map/util/Enums.java
@@ -0,0 +1,16 @@
+package com.zz.map.util;
+
+import java.util.Random;
+
+public class Enums {
+    //随机抽取一个Enum类中的实例
+    private static Random rand = new Random(47);
+
+    public static <T extends Enum<T>> T random(Class<T> ec){
+        return random(ec.getEnumConstants());
+    }
+
+    public static <T> T random(T[] values){
+        return values[rand.nextInt(values.length)];
+    }
+}
